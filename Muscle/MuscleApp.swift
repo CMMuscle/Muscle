57efa6973d1a9193e855d8d1e751871ef773a00d
kazua33 <88267594+kazua33@users.noreply.github.com>
2022-09-26 14:32:51 +0900

    Initial Commit

---
+++ Muscle/MuscleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleApp.swift
+//  Muscle
+//
+//  Created by 稗田一亜 on 2022/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MuscleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
